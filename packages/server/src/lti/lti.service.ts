46a9b682f4596825521855d4d6df64ce5e664309
Whiteknight07 <fiestyboxer@gmail.com>
2026-09-03 22:33:39 -0700

    fix(lti): derive cookie policy from public protocol

--- packages/server/src/lti/lti.service.ts
+++ packages/server/src/lti/lti.service.ts
@@ -13,7 +13,7 @@ import {
   Provider,
 } from '@bhunt02/lti-typescript';
 import { LMSCourseIntegrationModel } from '../lmsIntegration/lmsCourseIntegration.entity';
-import { ERROR_MESSAGES, isProd, Role } from '@koh/common';
+import { ERROR_MESSAGES, Role } from '@koh/common';
 import { JwtService } from '@nestjs/jwt';
 import { CookieOptions } from 'express';
 import { LtiCourseInviteModel } from './lti-course-invite.entity';
@@ -39,13 +39,14 @@ const LTI_MEMBERSHIP_STAFF_ROLES = [
   'http://purl.imsglobal.org/vocab/lis/v2/membership#Instructor',
   'http://purl.imsglobal.org/vocab/lis/v2/membership#TeachingAssistant',
 ];
+const useSecureCookies = process.env.DOMAIN?.startsWith('https://') ?? false;
 
 @Injectable()
 export class LtiService {
   static readonly cookieOptions: CookieOptions = {
     httpOnly: true,
-    secure: isProd(),
-    sameSite: isProd() ? 'none' : 'lax',
+    secure: useSecureCookies,
+    sameSite: useSecureCookies ? 'none' : 'lax',
   };
   constructor(
     private jwtService: JwtService,
